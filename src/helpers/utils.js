97d949d9b283ec677fb4c1da8e9eb95d7f7763cd
enotus-chang <anton.s.chegus@gmail.com>
2023-05-11 01:14:41 +0500

    Вынес функции в модуль utils, переосмыслил расчет продолжительности

---
+++ src/helpers/utils.js
@@ -0,0 +1,20 @@
+import dayjs from 'dayjs';
+
+const routeDateFormat = (date) => dayjs(date).format('DD MMM');
+const dateFormat = (date) => dayjs(date).format('YYYY-MM-DD');
+const timeFormat = (date) => dayjs(date).format('HH:mm');
+const dateTimeFormat = (date) => dayjs(date).format('YYYY-MM-DD[T]HH:mm');
+const durationFormat = (start, stop) => {
+  const gap = dayjs(stop).diff(dayjs(start));
+  let gapFormat = 'mm[m]';
+  if (gap >= 3600000) {
+    gapFormat = 'HH[h] mm[m]';
+  }
+  if (gap >= 86400000) {
+    gapFormat = 'DD[d] HH[h] mm[m]';
+  }
+  return dayjs(gap).format(gapFormat);
+};
+const totalPrice = (offers) => offers.reduce((acc, {price}) => acc + price, 0);
+
+export {routeDateFormat, dateFormat, timeFormat, dateTimeFormat, durationFormat, totalPrice};
