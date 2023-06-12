3c4ee5db9af3438ce9755cefd51543becf8981b5
enotus-chang <anton.s.chegus@gmail.com>
2023-06-13 02:57:21 +0500

    Исправил routePoints

--- src/presenter/content-presenter.js
+++ src/presenter/content-presenter.js
@@ -108,7 +108,7 @@ export default class ContentPresenter {
       this.#stubComponent = new StubView(this.#activeFilterType, this.#noRoutePoints);
       render(this.#stubComponent, this.#routeListComponent.element);
     } else {
-      this.#tripInfoComponent = new TripInfoView(routePoints, this.#offersData, this.#destinationsData);
+      this.#tripInfoComponent = new TripInfoView([...this.#routePointsModel.routePoints], this.#offersData, this.#destinationsData);
       render(this.#tripInfoComponent, this.#tripInfoContainer, RenderPosition.AFTERBEGIN);
 
       this.#renderRoutePoints(routePoints);
