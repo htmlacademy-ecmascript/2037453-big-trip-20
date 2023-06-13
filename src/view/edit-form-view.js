0558f5e6678692b026cb5b43ca44a64d4f6a48e4
enotus-chang <anton.s.chegus@gmail.com>
2023-06-14 00:06:54 +0500

    minor fix

--- src/view/edit-form-view.js
+++ src/view/edit-form-view.js
@@ -134,13 +134,8 @@ function createEditFormTemplate(routePoint, allOffers, allDestinations) {
                 <button class="event__reset-btn" type="reset"${isDisabled}>${resetButtonText}</button>
                 ${rollUpButtonMarkup}
               </header>
-              <section class="event__details">
-                <section class="event__section  event__section--offers">
-                  <h3 class="event__section-title  event__section-title--offers">Offers</h3>
-                  <div class="event__available-offers">
-                  ${eventOffersListMarkup.join('')}
-                  </div>
-                </section>
+              <section class="event__details">                
+                 ${eventOffersListMarkup}
                 ${createDestinationTemplate(allDestinations, destination)}
               </section>
             </form>
